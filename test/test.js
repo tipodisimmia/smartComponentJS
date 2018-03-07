5802b24dcfaf8ae25c8b855d928bb7630f9998be
Gilbert80 <roberto.rossi@gmail.com>
2018-03-07 17:55:31 +0100

    test commit

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 import {AppRegistry}  from "../src/index";
 var assert = require('assert');
+import jsdomify from 'jsdomify';
+jsdomify.create(
+    '<!DOCTYPE html><html><head></head><body>hello</body></html>'
+);
+const documentRef = jsdomify.getDocument();
 describe('Array', function() {
+    console.log(documentRef.getElementsByTagName("body"));
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
-            console.log(AppRegistry);
             assert.equal([1,2,3].indexOf(4), -1);
         });
     });
