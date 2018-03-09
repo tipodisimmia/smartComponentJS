6b772335de7a65a46b769dcc16f4e924d3924282
Gilbert80 <roberto.rossi@gmail.com>
2018-03-09 13:05:40 +0100

    ie11 fix, test progress

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ AppRegistry.registerComponents({TestComponent});
 
 let testComponent=null;
 let testComponent2=null;
+let testComponent3=null;
 
 describe('TestComponent1 - Instance by name', function() {
     testComponent = AppRegistry.initComponentByName(document.querySelector(`[component-reference-name="TestComponent1"]`),"TestComponent");
@@ -41,14 +42,26 @@ describe('TestComponent3 added dinamically - add dinamically TestComponent3 like
     it('TestComponent3 - should be present like child of TestComponent2', async function() {
         let testComponent2DomEl= document.querySelector(`[component-reference-name="TestComponent2"]`);
         var node=document.createElement('div');
-        node.innerHTML=`<div component="TestComponent"  component-reference-name="TestComponent3"></div>`;
+        node.innerHTML=`<div component="TestComponent"  component-reference-name="TestComponent3">
+            <button component-click="clickHandler()">TestComponent3 Click Handler</button>
+        </div>`;
         testComponent2DomEl.appendChild(node.childNodes[0]);
         testComponent2.loadChildComponents();
         await setTimeout(()=>{},500);
+        testComponent3=testComponent2.components["TestComponent3"];
         assert.equal(testComponent2.components["TestComponent3"].componentReferenceName, "TestComponent3");
     });
 });
 
+
+describe('TestComponent3 component-click - click on TestComponent child with component-click attribute', function() {
+    it('TestComponent3 - clickEventsNumber must be increase of one', async function() {
+        let clickEventsNumberBefore=TestManager.getClickEvents("TestComponent3");
+        document.querySelector(`[component-reference-name="TestComponent3"] [component-click="clickHandler()"]`).click();
+        await setTimeout(()=>{},500);
+        assert.equal(TestManager.getClickEvents("TestComponent3"), (clickEventsNumberBefore + 1));
+    });
+});
 
 //Destroy con detach listener
 //Init
