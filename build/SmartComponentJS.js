3fd39a7935db4d978f388166677505a5e005d48b
zibibbalibbo <benini.and@gmail.com>
2018-09-20 15:42:50 +0200

    Bugfix

--- build/SmartComponentJS.js
+++ build/SmartComponentJS.js
@@ -780,7 +780,7 @@ var SmartComponent = function () {
     }, {
         key: "_generateUid",
         value: function _generateUid() {
-            return this.params.className || this.constructor.name + "_" + 'xxxxxxxx'.replace(/[xy]/g, function (c) {
+            return (this.params.className || this.constructor.name) + "_" + 'xxxxxxxx'.replace(/[xy]/g, function (c) {
                 var r = Math.random() * 16 | 0,
                     v = c == 'x' ? r : r & 0x3 | 0x8;
                 return v.toString(16);
--- src/SmartComponent.js
+++ src/SmartComponent.js
@@ -88,7 +88,7 @@ class SmartComponent {
     }
 
     _generateUid() {
-        return  this.params.className || this.constructor.name+"_"+'xxxxxxxx'.replace(/[xy]/g, function (c) {
+        return  (this.params.className || this.constructor.name)+"_"+'xxxxxxxx'.replace(/[xy]/g, function (c) {
             var r = Math.random() * 16 | 0,
                 v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
